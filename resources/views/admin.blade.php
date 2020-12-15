30703c903defc92aebd01251c509581bd4791974
PiotrZon <piotrekzon1@gmail.com>
2020-12-15 20:28:57 +0100

    Panel admina poprawki

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,8 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-      
+
+<div class="container">  
 <h2 style="text-align:center">Witamy w panelu administratora</h2>
 
+<h4>Wszyscy użytkownicy</h4>
+
+<h4>Wszystkie eventy</h4>
+
+<h4>Dodaj event</h4>
+</div>
 
 @endsection
